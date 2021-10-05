84d575135009b7e1a0733361059ada3c92b01a60
Arınç Alp Eren <arinc.alp.98@gmail.com>
2021-10-05 23:29:49 +0300

    Revert "Delete says_hello_world.h"
    This reverts commit c0efc97ab937b2cd0b0dcccc8a578efa23c9de63.

---
+++ says_hello_world.h
@@ -0,0 +1,11 @@
+#ifndef SAYS_HELLO_WORLD
+#define SAYS_HELLO_WORLD
+#include <iostream>
+class SaysHelloWorld {
+	public:
+		void sayHelloWorld(){
+			std::cout << "I Say Hello World!";
+		}
+};
+
+#endif
