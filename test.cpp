3e62a03b6bbd12ba538b11799f6a3c2cd3aaff91
Arınç Alp Eren <arinc.alp.98@gmail.com>
2021-10-19 16:46:21 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,12 +3,12 @@
 
 SaysHelloWorld shw;
 
-TEST(SquareRootTest, PositiveNos) { 
-    ASSERT_GE(1, shw.printRandomNumber());
+TEST(RandomNumberTest, LessOrEqual) { 
+    ASSERT_LE(1, shw.printRandomNumber());
 }
 
-TEST(SquareRootTest, NegativeNos) {
-    ASSERT_LE(10, shw.printRandomNumber());
+TEST(RandomNumberTest, GreatOrEqual) {
+    ASSERT_GE(10, shw.printRandomNumber());
 }
 
 int main(int argc, char **argv) {
